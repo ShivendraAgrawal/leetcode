fece79219441a360ac6382a2ed9432ae1de10a1d
ShivendraAgrawal <shivendra.agrawal@gmail.com>
2018-01-13 15:06:55 -0700

    Add question 3

--- 3_longest-substring-without-repeating-characters.py
+++ 3_longest-substring-without-repeating-characters.py
@@ -4,7 +4,29 @@ class Solution:
         :type s: str
         :rtype: int
         """
+        if len(s) == 0:
+            return 0
+
+        start = 0
+        max_gap = 1
+
+        for i in range(1, len(s)):
+            end = i
+            j = i - 1
+            while (j >= start):
+                print("i = ",i , ", j = ", j)
+                if s[i] != s[j]:
+                    j -= 1
+                    gap = end - j
+                    if gap > max_gap:
+                        max_gap = gap
+                else:
+                    gap = end - start
+                    if gap > max_gap:
+                        max_gap = gap
+                    start = j + 1
+        return max_gap
 
 
 solution = Solution()
-print(solution.lengthOfLongestSubstring("abcabcbb"))
+print(solution.lengthOfLongestSubstring("abcabcab"))
